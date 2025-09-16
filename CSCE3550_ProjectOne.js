28adcdedb92d9ef813a92efaf63b46b65429990b
Thunder Oyebi <thunderoyebi@my.unt.edu>
2025-09-16 01:23:35 -0500

    Created a server for the project.

--- CSCE3550_ProjectOne.js
+++ CSCE3550_ProjectOne.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+//Import HTTP Module
+const HTTP = require('http');
+
+//Localhost for the server
+const NAMEHOST = '127.0.0.1';
+
+//Port the server listens to
+const PORT = 8080;
+
+//Creating a server
+const server = HTTP.createServer((req, res) => {
+    //Sets the request with a HTML Status and Content Type
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    //Sends the response
+    res.end('Hello World!\n');
+});
+
+//Server is listening to any connections
+server.listen(PORT, NAMEHOST, () => {
+    console.log(`Server running at http://${NAMEHOST}:${PORT}/`);
+});
