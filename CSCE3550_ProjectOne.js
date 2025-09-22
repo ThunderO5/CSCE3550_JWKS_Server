73735c3ab89dddca79fb86c9de70ee7731d90225
Thunder Oyebi <thunderoyebi@my.unt.edu>
2025-09-21 23:02:44 -0500

    Added the RSA private and public key functionality.

--- CSCE3550_ProjectOne.js
+++ CSCE3550_ProjectOne.js
@@ -1,22 +1,52 @@
+//Section 1 - RSA Stuff
+const CRYPTO = require("crypto"); //Allows for RSA implementation
+const {v4 : uuidv4} = require("uuid"); //The unique ID
+
+//Generate new RSA key pair
+function generateKeyPair()
+{
+    //Sets up the public and private pairs
+    const {publicKey, privateKey} = CRYPTO.generateKeyPairSync("rsa", 
+    {
+        modulusLength: 2048,
+        publicKeyEncoding: {type: "skpi", format: "pem"},
+        privateKeyEncoding: {type: "pkcs8", format: "pem"}
+    });
+
+    //returns the unique id, public and private keys, and expiration date
+    return {
+        kid: uuidv4(), //Unique id
+        publicKey,
+        privateKey,
+        expiresAt: Date.now + 5 * 60 * 1000 //expires in 5 minutes
+    };
+}
+
+//Section 2 - Public Key to JWKS Format
+
+//Section 3 - Server Stuff
 //Import HTTP Module
 const HTTP = require('http');
 
-//Localhost for the server
-const NAMEHOST = '127.0.0.1';
+//Hostname for the server
+const HOSTNAME = '127.0.0.1';
 
-//Port the server listens to
+//The server's port for incoming requests
 const PORT = 8080;
 
-//Creating a server
+//Server is created
 const server = HTTP.createServer((req, res) => {
     //Sets the request with a HTML Status and Content Type
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    //Writes into the website
+    res.write("Hello World!");
 
-    //Sends the response
-    res.end('Hello World!\n');
+    //Ends the response
+    res.end();
 });
 
-//Server is listening to any connections
-server.listen(PORT, NAMEHOST, () => {
-    console.log(`Server running at http://${NAMEHOST}:${PORT}/`);
+//Server listens to any connections
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 });
